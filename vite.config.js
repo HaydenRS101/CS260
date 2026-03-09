fc39d820875e03eb4393bc60f2cb69bf054fc2e1
Hayden <haydenrsmith06@gmail.com>
2026-03-09 17:43:42 -0600

    Add gitignore, recommited other changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
-})
+  server: {
+    proxy: {
+      '/api': 'http://localhost:4000',
+    },
+  },
+});
