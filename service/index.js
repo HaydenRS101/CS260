890ad9fbcf05e3ca086ab69ce341de7de179e34f
Hayden <haydenrsmith06@gmail.com>
2026-03-09 18:29:15 -0600

    Added helper function to handle cookies, and a lot of dictionaries for users information.

--- service/index.js
+++ service/index.js
@@ -17,3 +17,32 @@ app.use(cookieParser());
 
 //when the files are public, serve them automatically
 app.use(express.static('public'));
+
+//stores user info. Bycrypt automatically encrypts the password. 
+const users = {};
+
+//this stores a persons token which we save and pull back up later so we know the info they have etc.
+const sessions = {};
+
+//maps the user to the events
+const scheduleEvents = {};
+
+//shared goals from other users in an array
+//includes examples
+const communityGoals = [
+  { id: uuid.v4(), user: 'Sam', goal: 'Run a 5K this spring' },
+  { id: uuid.v4(), user: 'Jacob', goal: 'Journal every night before bed' }
+];
+
+
+
+//the following bit looks up who is logged in based on the cookie
+function getLoggedInUser(req) {
+  const token = req.cookies.token;
+  if (!token) return null;
+  return sessions[token] || null;
+}
+
+
+
+
