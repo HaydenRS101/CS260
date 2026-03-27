d305bc8e04e2f2f8a938a64284e87cabaf97343d
Hayden <haydenrsmith06@gmail.com>
2026-03-27 00:01:29 -0600

    Fixed other things, saved everything just in case I missed in an eralier commit

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { useWebSocket } from '../usewebsocket';
+import { useWebSocket } from '../useWebSocket';
 
 
 export function Home() {
